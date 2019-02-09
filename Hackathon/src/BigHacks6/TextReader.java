37fe21d310682d479eeba4742678b1c4b8e468ac
ViNguyen15 <fluffywin@gmail.com>
2019-02-09 15:11:16 -0500

    new program

---
+++ Hackathon/src/BigHacks6/TextReader.java
@@ -0,0 +1,29 @@
+package BigHacks6;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
+public class TextReader {
+	public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+		
+		// text file
+		File pseudoFile = new File("pseudo.txt");
+		// Scanner read data from file
+		Scanner pseudoReader = null;
+
+		try {
+			pseudoReader = new Scanner(pseudoFile);
+			//System.out.println("It Works!");
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("No file is found- folder is empty");
+		}
+		
+		while (pseudoReader != null && pseudoReader.hasNext()) {
+			String sop = pseudoReader.next();
+			if (sop.equalsIgnoreCase("print")){
+				System.out.println("System.out.println(\"" + pseudoReader.nextLine() + "\");");
+			}
+		}
+	}
+}
