6410cc4d3dcbe67493fb35280c6d859011cc91de
Daksh Patel <dakshpatel6@gmail.com>
2019-02-10 07:46:52 -0500

    Updated loops

--- WritingLoops.java
+++ WritingLoops.java
@@ -2,11 +2,51 @@
 public class WritingLoops extends pseudoPrint{
 	
 	
-	public static String loops(String a, String b, String c, String d,String e)
+	
+	public static String iterators(String a)
+	{
+		String iterator="";
+		String y = "";
+		
+		String [] iterators = {"x++","x=x+1","x--","x=x-1"};
+		
+		for(int i = 0;i<iterators.length;i++)
+		{
+			
+			if(iterators[i].equalsIgnoreCase(a))
+			{
+				
+				if(a.equals(iterators[0])||a.equals(iterators[1]))
+				{
+					
+					iterator = "x++";
+					
+				}
+				else if(a.equals(iterators[2])||a.equals(iterators[3]))
+				{
+					iterator = "x--";
+					
+				}
+				
+			}
+			
+					
+		}
+		
+		
+		return y= iterator;
+	}
+	
+	public static String loops(String a, String b, String c, String d,String e,String f)
 	{
 		
 		String loop = b;
-		String[] looptyp = { "for", "while", "do"};
+		String x="";
+		String y="";
+		String z="";
+		String L = "";
+		
+		String[] looptyp = { "for"};
 		
 		for(int i = 0;i<looptyp.length;i++)
 		{
@@ -21,8 +61,6 @@ public class WritingLoops extends pseudoPrint{
 					case "for":
 						
 					{
-						
-						//"for("
 						
 						{
 							int space = b.indexOf(" ");						
@@ -39,51 +77,53 @@ public class WritingLoops extends pseudoPrint{
 							bfix=b.replace(initialize, " ");
 							b=bfix.trim();
 							
-							//reader(datatype,var,initialize,null);
+							x=reader(datatype,var,initialize,null);
 							
-						
-							int grt = c.indexOf("<");
-							int les = c.indexOf(">");
 							
-							String x = String.valueOf(c.charAt(grt+1));
-							String y = String.valueOf(c.charAt(les+1));
+							space = c.indexOf(" ");	
+							String lol = c.substring(0,space);
+							String cfix =c.replace(lol, " ");
+							c= cfix.trim();	
 							
-							if(x == "="|| y== "=")
-							{
-								c.substring(grt,grt+1);
-								c.substring(les,les+1);
-							}
-							else
-							{
-								String s = String.valueOf(c.charAt(grt));
-								String t = String.valueOf(c.charAt(les));
-								
-								System.out.println(s);
-								System.out.println(t);
-								
-							}
+							space = c.indexOf(" ");	
+							String op = c.substring(0,space);
+							cfix = c.replace(op, " ");
+							c = cfix.trim();
 							
-							//operations(var,,value);
+						 	space = c.indexOf(" ");	
+							String range = c.substring(0,c.length());
+							cfix = c.replace(range, " ");
+							c = cfix.trim();
 							
+							
+							y=operations(var,op,range);
+							
+							
+							z = d;
+							
+							L = (a+"("+x+y+";"+z+")\n{\n\t"+f+"\n}\n");	
+									
 						}
-						
+							
+				
 					}
-						
+					
 				}
+						
 			}
 			
-			
 		}
-		
-		
-		
-		return "";
-	}
+
+		return L;			
+}
+	
 
 	public static void main(String[] args)
 	
 	{
-		loops("for","int x = 0", "x<10","x++","then");
+		String w = reader("print", "x",null,null);
+		
+		System.out.print(loops("for","int x = 0", "x < 10","x++","then",w));
 	}
 
 }
